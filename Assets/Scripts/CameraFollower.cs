9794bfe89a5bc61b462d4094db7f1a1b1df1e876
Technifutur <600@technifutur.be>
2018-01-31 12:46:54 +0100

    Improved gameplay and camera following

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    private float offset;
+    public GameObject player;
+
+	void Start ()
+    {
+        offset = transform.position.y - player.transform.position.y;
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/RecyclePlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecyclePlatform : MonoBehaviour
+{
+    public Camera gameCamera;
+    public GameObject platform;
+    private Transform platformPosition;
+
+	void Start ()
+    {
+        platformPosition = GetComponent<Transform>();
+	}
+	
+	void Update ()
+    {
+		if (transform.position.y < gameCamera.transform.position.y - 20f)
+        {
+            platformPosition.position = new Vector3((Random.value * 16f) - 8f, 0f, 0f);
+        }
+	}
+}
